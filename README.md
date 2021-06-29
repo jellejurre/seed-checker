8a4f002d8048956ed019d02d0b028422fa157935
Jelle Groenendijk <j.j.h.groenendijk@student.tudelft.nl>
2021-06-29 18:22:38 +0200

    test: Add crash tests to test stability

--- README.md
+++ README.md
@@ -9,5 +9,9 @@ Due to technical and legal limitations, we can't publish our project with mapped
 4: clone this SeedChecker repository  
 5: code in this repository (sadly, publishing it and importing it will remove class names, which is possible to work with, but very difficult)
 
+p.s. At the start of this program, you should run `SeedCheckerSettings.initialise();`.  This is not 100% required, but it might fix weird heisenbugs.
+
+p.p.s. If you are getting an OutOfMemoryError, you might have to give the JVM more heap space by using -Xmx1024M as a JVM option. If you multithread, using this option will definitely increase speed as well.  
+Another way to fix this issue is to use fewer threads.
 ## Credit
 Made by [jellejurre](https://github.com/jellejurre) (discord: @jellejurre#8585) and [dragontamerfred](https://github.com/KalleStruik) (discord: @dragontamerfred#2779)
---
+++ src/test/java/CrashTest.java
@@ -0,0 +1,69 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.ThreadPoolExecutor;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import java.util.List;
+import nl.jellejurre.seedchecker.SeedChecker;
+import nl.jellejurre.seedchecker.SeedCheckerSettings;
+import org.junit.jupiter.api.Test;
+
+public class CrashTest {
+    public static ConcurrentHashMap<Long, Long> count = new ConcurrentHashMap<>();
+
+    @Test
+    public void singleThreadedTest(){
+        try {
+            SeedCheckerSettings.initialise();
+            for (int i = 0; i < 10; i++) {
+                long worldseed = ThreadLocalRandom.current().nextLong(Long.MAX_VALUE);
+                SeedChecker checker = new SeedChecker(worldseed);
+                BlockPos spawn = checker.getSpawnPos();
+                Box box = new Box(spawn.getX() - 48, 40, spawn.getZ() - 48, spawn.getX() + 48, 80,
+                    spawn.getZ() + 48);
+                Map<BlockPos, BlockEntity> list = checker.getBlockEntitiesInBox(box);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void multiThreadedTest(){
+        SeedCheckerSettings.initialise();
+        ThreadPoolExecutor pool = (ThreadPoolExecutor) Executors.newFixedThreadPool((int)Math.ceil(Runtime.getRuntime().availableProcessors()*1/4d));
+        for (int i = 0; i < 10; i++) {
+            pool.execute(new TestTask());
+        }
+        while(count.size()<10){
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public class TestTask implements Runnable{
+        @Override
+        public void run() {
+            long worldseed = ThreadLocalRandom.current().nextLong(Long.MAX_VALUE);
+            SeedChecker checker = new SeedChecker(worldseed);
+            BlockPos spawn = checker.getSpawnPos();
+            Box box = new Box(spawn.getX()-48, 40, spawn.getZ()-48, spawn.getX()+48, 80, spawn.getZ()+48);
+            Map<BlockPos, BlockEntity> list = checker.getBlockEntitiesInBox(box);
+            count.put(worldseed, worldseed);
+        }
+    }
+}
+
